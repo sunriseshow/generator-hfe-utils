ddf47ab04e0bec97e9b59c200998bf7e8371ca86
maquan <maquan@meituan.com>
2015-12-26 14:34:55 +0800

    update

--- gulp/tasks/jsmin.js
+++ gulp/tasks/jsmin.js
@@ -5,7 +5,6 @@ module.exports = function (gulp, Plugin, config) {
 	gulp.task('hfe-jsmin',['hfe-copyto'], function () {
 		gulp.src('src/**/*.js')
         .pipe(Plugin.jsmin())
-        .pipe(Plugin.rename({suffix: '.min'}))
         .pipe(gulp.dest('build'));
 	})
 };
--- gulp/tasks/offline.js
+++ gulp/tasks/offline.js
@@ -1,10 +1,15 @@
 'use strict';
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 module.exports = function (gulp, Plugin, config) {
 	//生成离线包
 	gulp.task('zip', function () {
-		var p = (process.cwd() + '/build.sh');
-		var gulpzip = spawn(p);
+		var repoInfoJSON = Plugin.repoInfoJSON,
+			group = repoInfoJSON.group,
+			name = repoInfoJSON.name,
+			version = repoInfoJSON.version;
+
+		var p = (process.cwd() + '/build.sh') + ' ' + group + ' ' + name + ' ' + version;
+		var gulpzip = exec(p);
 		gulpzip.stdout.pipe(process.stdout)
 	});
 };
