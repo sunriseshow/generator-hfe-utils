398365755c12e26ee6941e26089c0d60215973a7
maquan <maquan@meituan.com>
2016-01-04 11:50:19 +0800

    add jsmin rename

--- gulp/tasks/jsmin.js
+++ gulp/tasks/jsmin.js
@@ -5,6 +5,7 @@ module.exports = function (gulp, Plugin, config) {
 	gulp.task('hfe-jsmin',['hfe-copyto'], function () {
 		gulp.src('src/**/*.js')
         .pipe(Plugin.jsmin())
+        .pipe(Plugin.rename({suffix: '.min'}))
         .pipe(gulp.dest('build'));
 	})
 };
