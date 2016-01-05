ffb5a05435768757e712ed28e736409f3bb18a66
maquan <maquan@meituan.com>
2016-01-05 16:48:40 +0800

    update

--- gulp/tasks/cssmin.js
+++ gulp/tasks/cssmin.js
@@ -1,10 +1,9 @@
 'use strict';
 
 module.exports = function (gulp, Plugin, config) {
-	gulp.task('hfe-cssmin',['hfe-copyto'], function () {
+	gulp.task('hfe-cssmin',['hfe-copyto','hfe-sass'], function () {
 		return gulp.src('src/**/*.css')
         .pipe(Plugin.cssmin())
-        .pipe(Plugin.rename({suffix: '.min'}))
         .pipe(gulp.dest('build'));
 	})
 };
