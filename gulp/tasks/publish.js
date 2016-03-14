0d5aa2a250626b983e9fadd32ec1233a4ec59218
maquan <maquan@meituan.com>
2016-03-14 19:43:36 +0800

    add publish strong constaint: forbidden repeat publish

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -7,41 +7,56 @@ module.exports = function (gulp, Plugin, config) {
     var gulpConfig = require(path.join(__dirname, '../gulp.config.js'));
     var command = gulpConfig.exec;
     gulp.task('publish', function () {
-        var gulpBuild = exec('gulp build', function (gberr, gbout, gb) {
-            if (gberr) {
-                console.log(gberr);
-            } else {
-                Plugin.repoInfoJSON.timestamp = +new Date();
-                fs.writeJson('./repo-info.json', Plugin.repoInfoJSON, function (err) {
-                    if (err) {
-                        console.log(err.red);
+        var version = Plugin.repoInfoJSON.version;
+        var currentTag = 'awp/publish/' + version;
+
+        exec('git tag', function (err, stdout, stderr) {
+            if (err) {
+                console.log(err);
+            }
+            if (stdout.indexOf(currentTag) === -1) {
+                var gulpBuild = exec('gulp build', function (gberr, gbout, gb) {
+                    if (gberr) {
+                        console.log(gberr);
                     } else {
-                        exec('git rev-parse --abbrev-ref HEAD', function (e, out, se) { // o ==> daily/0.1.0 当前分支名
-                            var o = out.replace(/\r|\n/ig, '');
-                            exec(command.publish(o, '分支 ' + o + ' 上线'), function (err, stdout, stderr) {
-                                var msg = '命令 >>> ' + command.prepub(o, '发布分支' + o) + ' <<< 的执行结果：';
-                                console.log(msg.green);
-                                if (err) {
-                                    console.log(err);
-                                } else {
-                                    console.log(stdout);
-                                    console.log(stderr);
-                                    var gulpJudge = spawn('gulp', ['hfe-judgetag', '--env', 'publish']);
-                                    gulpJudge.stdout.pipe(process.stdout);
-                                    gulpJudge.stderr.pipe(process.stderr);
-                                    gulpJudge.on('close', function (code) {
-                                        if (code === 0) {
-                                            gulp.run('hfe-publish:html');
+                        Plugin.repoInfoJSON.timestamp = +new Date();
+                        fs.writeJson('./repo-info.json', Plugin.repoInfoJSON, function (err) {
+                            if (err) {
+                                console.log(err.red);
+                            } else {
+                                exec('git rev-parse --abbrev-ref HEAD', function (e, out, se) { // o ==> daily/0.1.0 当前分支名
+                                    var o = out.replace(/\r|\n/ig, '');
+                                    exec(command.publish(o, 'publish branch: ' + o), function (err, stdout, stderr) {
+                                        var msg = '\nThe following command is executed:\n> ' + command.prepub(o, 'publish branch: ' + o) + '\nplease wait.';
+                                        console.log(msg.green);
+                                        if (err) {
+                                            console.log(err);
+                                        } else {
+                                            console.log(stdout);
+                                            console.log(stderr);
+                                            var gulpJudge = spawn('gulp', ['hfe-judgetag', '--env', 'publish']);
+                                            gulpJudge.stdout.pipe(process.stdout);
+                                            gulpJudge.stderr.pipe(process.stderr);
+                                            gulpJudge.on('close', function (code) {
+                                                if (code === 0) {
+                                                    gulp.run('hfe-publish:html');
+                                                }
+                                            });
                                         }
                                     });
-                                }
-                            });
+                                });
+                            }
                         });
                     }
                 });
+                gulpBuild.stdout.pipe(process.stdout);
+                gulpBuild.stderr.pipe(process.stderr);
+            } else {
+                console.log("Oops, there is something wrong! \nTo confirm that this branch has not been published, or you can run `gulp newbranch` to switch to a new branch to publish!\nHave problems? concat @maquan".red);
             }
         });
-        gulpBuild.stdout.pipe(process.stdout);
-        gulpBuild.stderr.pipe(process.stderr);
+
+
+
     });
 };
